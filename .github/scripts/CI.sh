eb5a32e5fbc56774f58b16edca21f2f961a7385c
Toshinori Takahashi <dr.t.takahashi.0917@gmail.com>
2021-08-31 22:46:50 +0900

    feat: Add github actions workflow.

---
+++ .github/scripts/CI.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "exec 'python -m isort . --check --diff'"
+python -m isort . --check --diff
+echo
+
+echo "exec 'python -m black . --check --diff'"
+python -m black . --check --diff
+echo
+
+echo "exec 'python -m mypy sample tests --config-file ./mypy.ini'"
+python -m mypy sample tests --config-file ./mypy.ini
+echo
+
+echo "exec 'python -m pylint sample tests --rcfile=./.pylintrc'"
+python -m pylint sample tests --rcfile=./.pylintrc
+echo
+
+echo "exec 'python -m pytest tests -v --cov=sample --cov-branch'"
+python -m pytest tests -v --cov=sample --cov-branch
+echo
